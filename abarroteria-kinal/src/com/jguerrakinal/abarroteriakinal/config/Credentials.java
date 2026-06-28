91d8194895416ede7db15f561385a37bca2ea2df
jonathanandreguerra-alt <jonathanandreguerra@gmail.com>
2026-06-28 15:07:48 -0600

    Mejor Guianza

--- abarroteria-kinal/src/com/jguerrakinal/abarroteriakinal/config/Credentials.java
+++ abarroteria-kinal/src/com/jguerrakinal/abarroteriakinal/config/Credentials.java
@@ -3,8 +3,8 @@ package com.jguerrakinal.abarroteriakinal.config;
 public class Credentials {
     
         public static final String URL_DB = "jdbc:mysql://localhost:3306/abarroteria_kinal_in4bv";
-        public static final String USER_DB = "root";
-        public static final String PASS_DB = "8486";
+        public static final String USER_DB = "su usuario va aqui (root o admin)";
+        public static final String PASS_DB = "su contraseña va aqui";
         
         private Credentials(){
             
